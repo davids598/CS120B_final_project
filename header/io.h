92d220d14b0731b0b882fe682276b107f1ed8cd8
David <davidstrathman@gmail.com>
2021-12-08 00:15:06 -0800

    Added code to try and get custom characters working, failed so far

--- header/io.h
+++ header/io.h
@@ -8,4 +8,5 @@ void LCD_WriteData(unsigned char Data);
 void LCD_Cursor (unsigned char column);
 void LCD_DisplayString(unsigned char column ,const unsigned char *string);
 void delay_ms(int miliSec);
+void LCD_Custom_Char (unsigned char loc, unsigned char *msg);
 #endif
